ab0191c1c7f88097527b0d119683b495d4ef98e5
Juan Lauredo <juanlauredo@Juans-MBP.hsd1.fl.comcast.net>
2020-07-24 08:35:53 -0400

    updated

--- index.js
+++ index.js
@@ -1,36 +1,82 @@
-function atError(message){
-    let errorBox = document.querySelector(".alert");
-    errorBox.classList.contains("d-none") && errorBox.classList.toggle("d-none");
+var formisvalid = true;
 
+function atError(message) {
+  var errorBox = document.querySelector(".alert");
+  var errorTxt = document.querySelector(".errorText");
+
+  // var titleAlert = document.querySelector(".headerAlert");
+  errorBox.classList.contains("d-none") && errorBox.classList.toggle("d-none");
+
+  errorTxt.innerHTML += "<p>" + message + "</p>";
 }
 
-
-
 const validate = (e) => {
-    e.preventDefault();
-    let errorBox = document.querySelector("#exampleAccount");
-    !errorBox.classList.contains("d-none") && errorBox.classList.toggle("d-none");
-    errorBox.innerHTML = "";
+  e.preventDefault();
+  var errorBox = document.querySelector(".alert");
+  var errorTxt = document.querySelector(".errorText");
+  !errorBox.classList.contains("d-none") && errorBox.classList.toggle("d-none");
+  errorTxt.innerHTML = "";
 
-    let accField = document.querySelector("#exampleAccount");
-    let ccvField = document.querySelector("#exampleCvc");
-    let amountField = document.querySelector("#exampleCvc");
+  document.querySelectorAll("input").forEach(function (items) {
+    items.classList.remove("is-invalid");
+  });
+  document.querySelector("#exampleState").classList.remove("is-invalid");
 
+  var accField = document.querySelector("#exampleAccount");
+  var ccvCode = document.querySelector("#exampleCcv");
+  var amt = document.querySelector("#exampleAmount");
+  var cit = document.querySelector("#exampleCity");
+  var ste = document.querySelector("#exampleState");
+  var zip = document.querySelector("#exampleZip");
+  var firstN = document.querySelector("#exampleFirst");
+  var lastN = document.querySelector("#exampleLast");
 
-    if (accField.value < 100000000000){
-        atError("credit card number not valid")
-        accField.classList.add("is invalid")
-    }
-    if (ccvField.value.length < 3){
-        atError("credit card number not valid")
-        ccvField.classList.add("is-invalid")
-    }
-
-
-
+  if (accField.value.length < 16) {
+    atError("credit card number needs to be 16 digits");
+    accField.classList.add("is-invalid");
+    formisvalid = false;
+  }
+  if (ccvCode.value.length < 3) {
+    atError("ccv should be at least three digits");
+    ccvCode.classList.add("is-invalid");
+    formisvalid = false;
+  }
+  if (amt.value <= 0) {
+    atError("put in amount");
+    amt.classList.add("is-invalid");
+    formisvalid = false;
+  }
+  if (cit.value.length < 2) {
+    atError("enter city");
+    cit.classList.add("is-invalid");
+    formisvalid = false;
+  }
+  if (ste.value === "Pick a state") {
+    atError("select state");
+    ste.classList.add("is-invalid");
+    formisvalid = false;
+  }
+  if (zip.value.length < 5) {
+    atError("postal code should be 5 digits");
+    zip.classList.add("is-invalid");
+    formisvalid = false;
+  }
+  if (firstN.value.length < 2) {
+    atError("enter first name");
+    firstN.classList.add("is-invalid");
+    formisvalid = false;
+  }
+  if (lastN.value.length < 2) {
+    atError("enter last name");
+    lastN.classList.add("is-invalid");
+    formisvalid = false;
+  }
+  if (formisvalid === true) {
+    document.querySelectorAll("input").forEach(function (items) {
+      items.value = "";
+    });
+    document.querySelector("#exampleState").value = "Pick a state";
+  }
 };
 
-
-document.querySelector("#theForm").addEventListener("click", validate);
-
-
+document.querySelector("#theForm").addEventListener("submit", validate);
